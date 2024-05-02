7ba8a70da36916f2b86dfc6c3f9751399de8d4d7
aaronkerckhoff <aaronkerckhoff@gmail.com>
2024-05-02 14:21:24 +0200

    feat: add grep telescope search

--- lua/alesius/lazy/telescope.lua
+++ lua/alesius/lazy/telescope.lua
@@ -13,5 +13,6 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
     end
 }
