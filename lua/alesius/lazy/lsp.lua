57031198e10549d65e63ec2153459dd0ea94e9ab
aaronkerckhoff <aaronkerckhoff@gmail.com>
2025-02-09 17:20:11 +0100

    feat(lsp): change python lsp to `pyright`

--- lua/alesius/lazy/lsp.lua
+++ lua/alesius/lazy/lsp.lua
@@ -29,7 +29,7 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
-                "pylsp",
+                "pyright",
                 "jdtls",
                 "ts_ls",
             },
