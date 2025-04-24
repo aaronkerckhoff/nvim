465ff634dd4a5bb4fd53e7973ed6f1b83ffe050e
Aaron Kerckhoff <aaronkerckhoff@gmail.com>
2025-04-24 02:21:54 +0200

    feat: use terminal default colors

--- lua/alesius/lazy/colors.lua
+++ lua/alesius/lazy/colors.lua
@@ -4,13 +4,8 @@ return {
     priority = 1000,
     config = function()
         require('catppuccin').setup({
-            transparent_background = false,
+            transparent_background = true,
             no_italic = true,
-            color_overrides = {
-                all = {
-                    base = '#181818'
-                }
-            }
         });
 
         vim.cmd("colorscheme catppuccin")
